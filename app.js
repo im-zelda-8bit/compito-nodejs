b3c0c9dae0d9af32a0cb728c8dcafbe46c67d23b
Alessandro <alessandroromano2005@gmail.com>
2022-12-06 19:31:37 +0100

    Added hangman and tutorials
    è l'ultimo (forse)

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ function requestHandler(request, response) {
 
     let objurl = url.parse(request.url, "false");
     const path = objurl.pathname;
+    console.log(path);
     switch (path) {
         case '/':
             fs.readFile('index.html', function (error, data) {
@@ -96,7 +97,227 @@ function requestHandler(request, response) {
                 }
                 response.end();
             });
-            break;   
+            break;
+        
+        case '/sfondoslot':
+            fs.readFile('img/sfondo_slot.jpeg', function (error, data) {
+                if (error) {
+                    response.writeHead(404);
+                }
+                else {
+                    response.writeHead(200, { "content-Type": "img/jpeg" });
+                    response.write(data, "utf8");
+                }
+                response.end();
+            });
+            break;
+        
+        case '/tutorialslot':
+            fs.readFile('img/tutorial_slot.png', function (error, data) {
+                if (error) {
+                    response.writeHead(404);
+                }
+                else {
+                    response.writeHead(200, { "content-Type": "img/png" });
+                    response.write(data, "utf8");
+                }
+                response.end();
+            });
+            break;
+        case '/tutorialhangman':
+            fs.readFile('img/tutorial_hangman.png', function (error, data) {
+                if (error) {
+                    response.writeHead(404);
+                }
+                else {
+                    response.writeHead(200, { "content-Type": "img/png" });
+                    response.write(data, "utf8");
+                }
+                response.end();
+            });
+            break;
+        case '/impiccato':
+            fs.readFile('hangman.html', function (error, data) {
+                if (error) {
+                    response.writeHead(404);
+                }
+                else {
+                    response.writeHead(200, { "content-Type": "text/html" });
+                    response.write(data, "utf8");
+                }
+                response.end();
+            });
+            break;
+        
+        case '/hangcss':
+            fs.readFile('css/hang_style.css', function (error, data) {
+                if (error) {
+                    response.writeHead(404);
+                }
+                else {
+                    response.writeHead(200, { "content-Type": "text/css" });
+                    response.write(data, "utf8");
+                }
+                response.end();
+            });
+            break;
+
+        case '/img0':
+            fs.readFile('img/0.png', function (error, data) {
+                if (error) {
+                    response.writeHead(404);
+                }
+                else {
+                    response.writeHead(200, { "content-Type": "img/png" });
+                    response.write(data, "utf8");
+                }
+                response.end();
+            });
+            break;
+        case '/img1':
+            fs.readFile('img/1.png', function (error, data) {
+                if (error) {
+                    response.writeHead(404);
+                }
+                else {
+                    response.writeHead(200, { "content-Type": "img/png" });
+                    response.write(data, "utf8");
+                }
+                response.end();
+            });
+            break;
+        case '/img2':
+            fs.readFile('img/2.png', function (error, data) {
+                if (error) {
+                    response.writeHead(404);
+                }
+                else {
+                    response.writeHead(200, { "content-Type": "img/png" });
+                    response.write(data, "utf8");
+                }
+                response.end();
+            });
+            break;
+        case '/img3':
+            fs.readFile('img/3.png', function (error, data) {
+                if (error) {
+                    response.writeHead(404);
+                }
+                else {
+                    response.writeHead(200, { "content-Type": "img/png" });
+                    response.write(data, "utf8");
+                }
+                response.end();
+            });
+            break;
+        case '/img4':
+            fs.readFile('img/4.png', function (error, data) {
+                if (error) {
+                    response.writeHead(404);
+                }
+                else {
+                    response.writeHead(200, { "content-Type": "img/png" });
+                    response.write(data, "utf8");
+                }
+                response.end();
+            });
+            break;
+        case '/img5':
+            fs.readFile('img/5.png', function (error, data) {
+                if (error) {
+                    response.writeHead(404);
+                }
+                else {
+                    response.writeHead(200, { "content-Type": "img/png" });
+                    response.write(data, "utf8");
+                }
+                response.end();
+            });
+            break;
+        case '/img6':
+            fs.readFile('img/6.png', function (error, data) {
+                if (error) {
+                    response.writeHead(404);
+                }
+                else {
+                    response.writeHead(200, { "content-Type": "img/png" });
+                    response.write(data, "utf8");
+                }
+                response.end();
+            });
+            break;
+        case '/img7':
+            fs.readFile('img/7.png', function (error, data) {
+                if (error) {
+                    response.writeHead(404);
+                }
+                else {
+                    response.writeHead(200, { "content-Type": "img/png" });
+                    response.write(data, "utf8");
+                }
+                response.end();
+            });
+            break;
+        case '/img8':
+            fs.readFile('img/8.png', function (error, data) {
+                if (error) {
+                    response.writeHead(404);
+                }
+                else {
+                    response.writeHead(200, { "content-Type": "img/png" });
+                    response.write(data, "utf8");
+                }
+                response.end();
+            });
+            break;
+        case '/img9':
+            fs.readFile('img/9.png', function (error, data) {
+                if (error) {
+                    response.writeHead(404);
+                }
+                else {
+                    response.writeHead(200, { "content-Type": "img/png" });
+                    response.write(data, "utf8");
+                }
+                response.end();
+            });
+            break;
+        case '/img10':
+            fs.readFile('img/10.png', function (error, data) {
+                if (error) {
+                    response.writeHead(404);
+                }
+                else {
+                    response.writeHead(200, { "content-Type": "img/png" });
+                    response.write(data, "utf8");
+                }
+                response.end();
+            });
+            break;
+        case '/img11':
+            fs.readFile('img/11.png', function (error, data) {
+                if (error) {
+                    response.writeHead(404);
+                }
+                else {
+                    response.writeHead(200, { "content-Type": "img/png" });
+                    response.write(data, "utf8");
+                }
+                response.end();
+            });
+            break;
+        case '/impiccatojs':
+            fs.readFile('hangman.js', function (error, data) {
+                if (error) {
+                    response.writeHead(404);
+                }
+                else {
+                    response.writeHead(200, { "content-Type": "text/javascript" });
+                    response.write(data, "utf8");
+                }
+                response.end();
+            });
+            break; 
     }
 }
 
---
+++ hangman.js
@@ -0,0 +1,71 @@
+
+const wordsList = ['ciao', 'casa', 'animale', "telefono", "cellulare"]
+
+var word = "";
+var wordSplitted = "";
+var encoded = "";
+var counterLettersWrong = 0;
+var lettersWrong = [];
+
+function guessLetter(letterNumber){
+    
+    let d = document.getElementsByClassName("letter")[letterNumber].value;
+    let i=0;
+    let indexes = [];
+
+    if(!word.includes(d)){
+      if(lettersWrong.includes(d)){
+        return;
+      }
+      counterLettersWrong+=1;
+
+      if(counterLettersWrong >= 11){
+        document.getElementById('currentState').innerHTML = "HAI PERSO";
+        document.getElementById('img').src = "/img11";
+        return;
+      }
+
+      //set correct image with counter
+      document.getElementById('img').src = "/img" + counterLettersWrong;
+      
+      //per vedere se la lettera schiacciata era già stata inserita
+      lettersWrong.push(d);
+      document.getElementById('currentState').innerHTML = "Non c'è la lettera " + d;
+
+    } else {
+        
+        for(i=0; i<word.length; i++)
+            if(word[i]===d){
+                indexes.push(i);
+            }
+    }
+
+    indexes.forEach( element => {
+      wordSplitted[element] = d;
+    })
+
+    
+  console.log(wordSplitted);
+  if(wordSplitted.join("") == word){
+    document.getElementById('currentState').innerHTML = "HAI VINTO";
+  }
+    document.getElementById('word').innerHTML = wordSplitted.join(" ");
+}
+
+function createLetterSpaces(){
+    let i;
+    let wordSelected = Math.floor(Math.random() * wordsList.length);
+    let wordToInsert = wordsList[wordSelected];
+    word = wordToInsert;
+  
+    for(i=0; i<wordToInsert.length; i++){
+        if(i-1<wordToInsert.length){
+            encoded+= "_ "; 
+        } else {
+            encoded+="_";
+        }
+    }
+  
+    wordSplitted = encoded.split(" ");
+    document.getElementById('word').innerHTML = encoded;
+}
